5aa05f5812d3b63d233f985eeeaadd77e0388059
MarkMwago2 <markmwago@gmail.com>
2019-07-01 10:58:54 +0300

    this commit will add getters for the animal class attributes

---
+++ src/main/java/models/Animals.java
@@ -0,0 +1,20 @@
+package models;
+
+public class Animals {
+    private int animals_id;
+    private String animals_name;
+
+    public int getAnimals_id() {
+        return animals_id;
+    }
+
+    public String getAnimals_name() {
+        return animals_name;
+    }
+
+    public Animals(int animals_id, String animals_name) {
+        this.animals_id = animals_id;
+        this.animals_name = animals_name;
+//        this.id = 0;
+    }
+}
