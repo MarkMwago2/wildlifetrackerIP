af8502deaad6fb879f411010c1a061f26d862bdb
MarkMwago2 <markmwago@gmail.com>
2019-07-03 12:31:19 +0300

    add an override method to delete in the DB

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,5 +1,5 @@
 package models;
 import org.sql2o.*;
 
-public class DB { public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlifetrackerIP", "mark", "87654321");
+public class DB { public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlifetrackerip", "mark", "87654321");
 }
