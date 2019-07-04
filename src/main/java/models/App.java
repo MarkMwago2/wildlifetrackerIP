fc65d6de1b42c6b2e1efee614d47e1b7c796cbb0
MarkMwago2 <markmwago@gmail.com>
2019-07-04 12:28:08 +0300

    create the route to the home page of the app

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -1,4 +1,20 @@
 package models;
+import static spark.Spark.*;
+import org.sql2o.Connection;
+import spark.ModelAndView;
+import spark.template.handlebars.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
-}
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        },new HandlebarsTemplateEngine());
+
+    }}
