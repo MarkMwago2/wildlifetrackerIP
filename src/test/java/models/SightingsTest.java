66057d208e2ba53da1f8d0de00a41c4dee303ddc
MarkMwago2 <markmwago@gmail.com>
2019-07-09 15:24:13 +0300

    add password to database to enable deployment

---
+++ src/test/java/models/SightingsTest.java
@@ -0,0 +1,17 @@
+//package models;
+//
+//import org.junit.Rule;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class SightingsTest {
+//    @Rule
+//    public DatabaseRules database = new DatabaseRules();
+//
+//    @Test
+//    public void Sighting_InstantiatesCorrectly_True() {
+//        Sightings newsighting = new Sightings("Allois", "Zone 1", 3);
+//        assertTrue(newsighting instanceof Sightings);
+//    }
+//}
